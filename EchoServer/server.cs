a7bdac5091e73243c78b70e1728b3394469317a3
Alexander Egeberg <55107481+AlexanderEgeberg@users.noreply.github.com>
2020-09-11 10:47:43 +0200

    simple socket program

---
+++ EchoServer/server.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace EchoServer
+{
+    class server
+    {
+        public static void StartUp()
+        {
+            int port = 7777;
+
+            TcpListener serverSocket = new TcpListener(IPAddress.Loopback, port);
+            serverSocket.Start();
+
+            TcpClient socket = serverSocket.AcceptTcpClient();
+
+            using (socket)
+            {
+
+                NetworkStream ns = socket.GetStream();
+                StreamReader reader = new StreamReader(ns);
+                StreamWriter writer = new StreamWriter(ns) { AutoFlush = true };
+
+                while (true)
+                {
+                    string serverMessage = Console.ReadLine();
+                    writer.WriteLine($"server: {serverMessage}");
+                    string userMessage = reader.ReadLine();
+                    Console.WriteLine($"user: {userMessage}");
+                }
+                Console.WriteLine("Server disconnected...");
+            }
+        }
+
+    }
+}
